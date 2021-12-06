0c3a8cf0f897dc914130651a5935f12b4844597f
Jonathan Ptricio Cevallos Mayorga <jonathancevv@gmail.com>
2021-12-06 13:43:25 -0500

    Hola commit

---
+++ ContratarProfesionales/src/main/java/com/example/ContratarProfesionales/ContratarProfesionalesApplication.java
@@ -0,0 +1,13 @@
+package com.example.ContratarProfesionales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContratarProfesionalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContratarProfesionalesApplication.class, args);
+	}
+
+}
---
+++ ContratarProfesionales/src/test/java/com/example/ContratarProfesionales/ContratarProfesionalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ContratarProfesionales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContratarProfesionalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
